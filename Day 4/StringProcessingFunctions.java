31be8e7dfa15d4d34726d55628cdb8cd08828e65
DanielAShurmer <thatblackshadowcat@outlook.com>
2019-06-06 16:53:32 +0100

    Final Commit For Day 4

--- Day 4/StringProcessingFunctions.java
+++ Day 4/StringProcessingFunctions.java
@@ -11,6 +11,18 @@ class StringProcessing{
 		System.out.println("There are " + (WordsFound + 1) + " words in your message.");
 	}
 
+	public int ReturnWordCount(String Msg){
+		int WordsFound = 0;
+		int Counter = 0;
+		while (Counter < Msg.length()){
+			if ((Msg.substring(Counter,Counter + 1)).equals(" ")){
+				WordsFound ++;
+			}
+			Counter ++;
+		}
+		return WordsFound;
+	}
+
 	public void WordSplitter(String Msg){
 		int Counter = 0;
 		String CurrentWord = "";
@@ -39,7 +51,7 @@ class StringProcessing{
 			else{
 				CurrentWord = Msg.substring(Counter,Counter + 1) + CurrentWord;
 			}
-			Counter -= 1;
+			Counter --;
 		}
 	}
 
@@ -58,17 +70,72 @@ class StringProcessing{
 			Counter ++;		
 		}
 	}
+
+	public void RemoveDuplicateWords(String Msg){
+		int WordsFound = ReturnWordCount(Msg);
+		String[] PreviousWords= new String[WordsFound];
+		boolean SameWord = false;
+		Msg = Msg + " ";
+		int Counter = 0;
+		String CurrentWord = "";
+
+		// Set an array with the same number of spaces as the number of words in the string, and set all values to empty strings.
+		int ArrayInitCounter = 0;
+		for (String ListWord : PreviousWords){
+			PreviousWords[ArrayInitCounter] = "";
+			ArrayInitCounter ++;
+		}
+
+		while (Counter < Msg.length()){
+
+			// If encountering a space in the string, check complete word against all previous found words.
+			if ((Msg.substring(Counter,Counter + 1)).equals(" ")){	
+				for (String CheckingWord : PreviousWords){
+				 	if (CheckingWord.equals(CurrentWord)){
+				 		SameWord = true;
+				 	}
+				}
+
+				 // If no matches found, print the word, and add it to the next empty slot in the found words array.
+				 if (SameWord == false){
+				 	System.out.print(CurrentWord + " ");
+
+				 	int SubCounter = 0;
+
+				 	for (String ListWord : PreviousWords){
+				 		if (ListWord == ""){
+				 			PreviousWords[SubCounter] = CurrentWord;
+				 			break;
+				 		}
+				 		SubCounter ++;
+				 	}
+				}
+
+				// Whether or not a match was found, reset word and match-found variables.
+				SameWord = false; 
+				CurrentWord = "";
+			}
+			// If encoutering a non-space character, add it to the current word.
+			else{
+				CurrentWord += Msg.substring(Counter,Counter + 1);
+			}
+			Counter ++;		
+		}
+	}
 }
 
 class StringProcessingFunctions{
 	public static void main(String XYZ[]){
 		StringProcessing S = new StringProcessing();
+		System.out.println("---Count Words---");
 		S.WordCount("This is a sentence with many words in it.");
-		System.out.println("--------------------------------------------------");
+		System.out.println("---Print One Word At A Time---");
 		S.WordSplitter("This is a sentence with many words in it.");
-		System.out.println("--------------------------------------------------");
+		System.out.println("---Print One Word At A Time - Starting From The Last Word---");
 		S.ReverseWordSplitter("This is a sentence with many words in it.");
-		System.out.println("--------------------------------------------------");
+		System.out.println("---Print One Word At A Time - With Each Word Backwards---");
 		S.ReverseLettersPerWord("This is a sentence with many words in it.");
+		System.out.println("---Print Words With Duplicate Words Removed---");
+		S.RemoveDuplicateWords("This is a is sentence with many is words with in a it.");
 	}
 }
